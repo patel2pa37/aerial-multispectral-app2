83dd5931b063e1daebcc551ff3607ce49e946a6a
parth patel <patel2pa@dukes.jmu.edu>
2019-10-13 22:36:53 -0400

    updated UI

--- src/Components/NavigationBar/SideDrawer.js
+++ src/Components/NavigationBar/SideDrawer.js
@@ -11,10 +11,28 @@ const sideDrawer = props => {
     <nav className="side-drawer">
       <ul>
         <li>
-          <a href="/">Products</a>
+          <a href="/"> Overlays</a>
+        </li>
+        <div>-----------</div>
+        <li>
+          <a > <input type="checkbox"/>OverLay 1</a>
+          <p>Description</p>
+          <p>Date </p>
         </li>
         <li>
-          <a href="/">Users</a>
+          <a> <input type="checkbox"/>Overlay 2</a>
+          <p>Description</p>
+          <p>Date </p>
+        </li>
+        <li>
+          <a> <input type="checkbox"/> Overlay 3</a>
+          <p>Description</p>
+          <p>Date </p>
+        </li>
+        <li>
+          <a> <input type="checkbox"/> Overlay 4</a>
+          <p>Description</p>
+          <p>Date </p>
         </li>
       </ul>
     </nav>
